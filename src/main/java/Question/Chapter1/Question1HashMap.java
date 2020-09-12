ea70369ed9ed3dc262234707524b0992721e9c92
James Thomas <codeactor@protonmail.com>
2020-09-12 14:10:59 +0100

    Add hashmap implementation

---
+++ src/main/java/Question/Chapter1/Question1HashMap.java
@@ -0,0 +1,34 @@
+package Question.Chapter1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question1HashMap {
+    private final Map<Character, Integer> counts;
+    private final String toCheck;
+    private char repeat;
+
+    public Question1HashMap(String input) {
+        this.toCheck = input;
+        this.counts = new HashMap<>();
+    }
+
+    public char firstRepeatCharacter() {
+        return repeat;
+    }
+
+    public boolean uniqueCharacters() {
+        for (int i = 0; i < toCheck.length(); i++) {
+            char c = toCheck.charAt(i);
+
+            if (!counts.containsKey(c)) {
+                counts.put(c, 1);
+            } else {
+                repeat = c;
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/Question/Chapter1/Question1HashMapTest.java
@@ -0,0 +1,70 @@
+package Question.Chapter1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class Question1HashMapTest {
+
+    @Test
+    public void findsIsUnique() {
+        String input = "reason";
+        Question1HashMap question1HashMap = new Question1HashMap(input);
+        assertTrue(question1HashMap.uniqueCharacters());
+    }
+
+    @Test
+    public void findIsNotUnique() {
+        String input = "doggies";
+        Question1HashMap question1HashMap = new Question1HashMap(input);
+        assertFalse(question1HashMap.uniqueCharacters());
+    }
+
+    @Test
+    public void firstRepeat() {
+        String input = "doggies";
+        Question1HashMap question1HashMap = new Question1HashMap(input);
+        assertFalse(question1HashMap.uniqueCharacters());
+        assertEquals('g', question1HashMap.firstRepeatCharacter());
+    }
+
+    @Test
+    public void firstRepeatNoRepeat() {
+        String input = "reason";
+        Question1HashMap question1HashMap = new Question1HashMap(input);
+        assertTrue(question1HashMap.uniqueCharacters());
+        assertEquals('\u0000', question1HashMap.firstRepeatCharacter());
+    }
+
+    @Test
+    public void firstRepeatUpperCase() {
+        String input = "DOGGIES";
+        Question1HashMap question1HashMap = new Question1HashMap(input);
+        assertFalse(question1HashMap.uniqueCharacters());
+        assertEquals('G', question1HashMap.firstRepeatCharacter());
+    }
+
+    @Test
+    public void firstRepeatMultiple() {
+        String input = "doomed";
+        Question1HashMap question1HashMap = new Question1HashMap(input);
+        assertFalse(question1HashMap.uniqueCharacters());
+        assertEquals('o', question1HashMap.firstRepeatCharacter());
+    }
+
+    @Test
+    public void firstRepeatMultipleMixedCase() {
+        String input = "Doomed";
+        Question1HashMap question1HashMap = new Question1HashMap(input);
+        assertFalse(question1HashMap.uniqueCharacters());
+        assertEquals('o', question1HashMap.firstRepeatCharacter());
+    }
+
+    @Test
+    public void firstRepeatWithNumbers() {
+        String input = "tech101";
+        Question1HashMap question1HashMap = new Question1HashMap(input);
+        assertFalse(question1HashMap.uniqueCharacters());
+        assertEquals('1', question1HashMap.firstRepeatCharacter());
+    }
+}
