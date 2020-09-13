152aa175f0d82e4704a6c66fae8c8b8aefec472a
James Thomas <codeactor@protonmail.com>
2020-09-13 10:57:38 +0100

    Rename param

--- src/main/java/Question/Chapter1/Question3.java
+++ src/main/java/Question/Chapter1/Question3.java
@@ -3,9 +3,9 @@ package Question.Chapter1;
 public class Question3 {
     private static final char[] spaceHold = {'0', '2', '%'};
 
-    public static String toURL(String string, int stringLength) {
+    public static String toURL(String string, int trueLength) {
         char[] chars = string.toCharArray();
-        int nextChar = stringLength - 1;
+        int nextChar = trueLength - 1;
         int moveTo = chars.length - 1;
 
         while (nextChar >= 0) {
