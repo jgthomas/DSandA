289dc37f12e91211c492e462b9f56dd94507d7a9
James Thomas <codeactor@protonmail.com>
2020-09-13 10:55:28 +0100

    Question 3 solutions

---
+++ src/main/java/Question/Chapter1/Question3.java
@@ -0,0 +1,27 @@
+package Question.Chapter1;
+
+public class Question3 {
+    private static final char[] spaceHold = {'0', '2', '%'};
+
+    public static String toURL(String string, int stringLength) {
+        char[] chars = string.toCharArray();
+        int nextChar = stringLength - 1;
+        int moveTo = chars.length - 1;
+
+        while (nextChar >= 0) {
+            if (Character.isWhitespace(chars[nextChar])) {
+                for (char c : spaceHold) {
+                    chars[moveTo] = c;
+                    moveTo--;
+                }
+            } else {
+                chars[moveTo] = chars[nextChar];
+                moveTo--;
+            }
+
+            nextChar--;
+        }
+
+        return new String(chars);
+    }
+}
